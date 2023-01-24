24e9f69182019b1fe6fbcc1bb3433920ad5f056b
susanto-tm <susanto.tm@gmail.com>
2023-01-23 22:07:29 -0500

    Changed url and baseUrl.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'HackPSU',
   tagline: '',
-  url: 'https://hack-psu.github.io',
-  baseUrl: '/docs',
+  url: 'https://hack-psu.github.io/docs',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
